7f24715175433ef618f5e431c61ca4b013a74532
Anne Ensign <anne.ensign@gmail.com>
2022-03-21 16:19:13 -0400

    Start refactoring db_auto.py

--- project/python/db_auto.py
+++ project/python/db_auto.py
@@ -5,130 +5,116 @@ import sqlite3
 import pandas as pd
 
 
-#-----------------------------------------------
-# Make datasets folder
-dataset_dir = 'datasets'
-
-try:
-    os.chdir("../")
-    mkdir = 'mkdir {0}'.format(dataset_dir)
-    os.system(mkdir)
-    print("Folder created successfully")
-except:
-    print("Directory already exists or there is another error")
-
-
-#-----------------------------------------------
-# Change directory
-os.chdir("csv")
-print("Directory changed to csv folder")
-
-
-#-----------------------------------------------
-# Copy CSVs into datasets folder
-src_dir = "."
-dst_dir = "../datasets"
-
-try:
-    for root, dirs, files in os.walk(src_dir):
-        for f in files:
-            if f.endswith('.csv'):
-                shutil.copy(os.path.join(root,f), dst_dir)
-            print(f"File '{f}' copied successfully")
-except:
-    print("There are no CSV files to copy")
-
-
-#-----------------------------------------------
-# Make list of CSVs
 csv_files = []
-
-for file in os.listdir(os.getcwd()):
-    if file.endswith('.csv'):
-        csv_files.append(file)
-
-
-#-----------------------------------------------
-# Read CSVs 
-data_path = '../'+dataset_dir+'/'
-
 df = {}
-for file in csv_files:
+
+def make_new_dir(dir_name):
     try:
-        df[file] = pd.read_csv(data_path+file)
-        print(f"{file} read successfully")
-    except UnicodeDecodeError:
-        df[file] = pd.read_csv(data_path+file, encoding="ISO-8859-1")
+        mkdir = 'mkdir {0}'.format(dir_name)
+        os.system(mkdir)
+        print(f"Directory '{dir_name}' created successfully")
+    except:
+        print(f"Directory '{dir_name}' already exists or there is another error")
 
 
-#-----------------------------------------------
-# Test df 
-print(df['Company Client List.csv'].head())
-print(df['Company Orders.csv'].head())
+def make_dataset_dir():
+    """Make datasets folder and copy CSVs"""
+    dataset_dir = 'datasets'
+    os.chdir("../")
+    make_new_dir(dataset_dir)
+
+    # Change directory
+    os.chdir("csv")
+
+    # Copy CSVs into datasets folder
+    src_dir = "."
+    dst_dir = "../datasets"
+
+    try:
+        for root, dirs, files in os.walk(src_dir):
+            for f in files:
+                if f.endswith('.csv'):
+                    shutil.copy(os.path.join(root,f), dst_dir)
+                print(f"File '{f}' copied successfully")
+    except:
+        print("There are no CSV files to copy")
 
 
-#-----------------------------------------------
-# Change directory
-os.chdir("..")
-os.listdir(os.getcwd())
+
+def make_df():
+    """Make dataframes from pandas"""
+    # Make list of CSVs
+    for file in os.listdir(os.getcwd()):
+        if file.endswith('.csv'):
+            csv_files.append(file)
+
+    # Read CSVs 
+    data_path = '../datasets/'
+
+    for file in csv_files:
+        try:
+            df[file] = pd.read_csv(data_path+file)
+            print(f"{file} read successfully")
+            print(f"df[{file}].head()")
+        except UnicodeDecodeError:
+            df[file] = pd.read_csv(data_path+file, encoding="ISO-8859-1")
 
 
-#-----------------------------------------------
-# Make database folder
-database_dir = 'database'
-
-try:
-    mkdir = 'mkdir {0}'.format(database_dir)
-    os.system(mkdir)
-    print("Folder created successfully")
-except:
-    print("Directory already exists or there is another error")
+def make_db_dir():
+    """Make a database directory"""
+    os.chdir("..")
+    database_dir = 'database'
+    make_new_dir(database_dir)
+    # To start make_db() in correct folder
+    os.chdir(database_dir)
 
 
-#-----------------------------------------------
-# Change directory
-os.listdir(os.getcwd())
-os.chdir(database_dir)
-
-
-#-----------------------------------------------
 # ETL to db
-for k in csv_files:    
-    dataframe = df[k]
-    
-    # Clean table name
-    clean_table_name = k.lower().replace(" ", "_").replace(r"/""\\", "_")
-    clean_table_name = re.sub(r'[^a-zA-Z0-9_.]','', clean_table_name)    
-    
-    #Clean table data
-    dataframe.columns = [x.lower().replace(" ", "_").replace(r"/""\\", "_").strip() for x in dataframe.columns]
-    dataframe.columns = dataframe.columns.str.replace('[^A-Za-z\s_.]+', '', regex=True)
-    
-    table_name = '{0}'.format(clean_table_name.split('.')[0])
-    
-    # Change data types
-    new_dtypes = {
-    'object': 'varchar',
-    'float64': 'float',
-    'int64': 'int',
-    'datetime64': 'timestamp',
-    'timedelta64[ns]': 'varchar'
-    }
-    
-    col_strings = ", ".join("{} {}".format(n, d) for (n, d) in zip(dataframe.columns, dataframe.dtypes.replace(new_dtypes)))
-    
-    # Write to db with SQLite
-    conn = sqlite3.connect('company.db')
-    c = conn.cursor()
+def make_db():
+    for k in csv_files:    
+        dataframe = df[k]
+        # Clean table name
+        clean_table_name = k.lower().replace(" ", "_").replace(r"/""\\", "_")
+        clean_table_name = re.sub(r'[^a-zA-Z0-9_.]','', clean_table_name)    
+        
+        #Clean table data
+        dataframe.columns = [x.lower().replace(" ", "_").replace(r"/""\\", "_").strip() for x in dataframe.columns]
+        dataframe.columns = dataframe.columns.str.replace('[^A-Za-z\s_.]+', '', regex=True)
+        
+        table_name = '{0}'.format(clean_table_name.split('.')[0])
+        
+        # Change data types
+        new_dtypes = {
+        'object': 'varchar',
+        'float64': 'float',
+        'int64': 'int',
+        'datetime64': 'timestamp',
+        'timedelta64[ns]': 'varchar'
+        }
+        
+        col_strings = ", ".join("{} {}".format(n, d) for (n, d) in zip(dataframe.columns, dataframe.dtypes.replace(new_dtypes)))
+        
+        # Write to db with SQLite
+        conn = sqlite3.connect('company.db')
+        c = conn.cursor()
 
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS table_name (
-    col_strings    
-    )
-    ''')
-    print('{0} was created successfully'.format(table_name))
-    conn.commit()
-    
-    dataframe.to_sql(table_name, conn, if_exists='replace', index=False)
-    
-    conn.close()
+        c.execute(f'''
+        CREATE TABLE IF NOT EXISTS {table_name} (
+        {col_strings}    
+        )
+        ''')
+        print('{0} was created successfully'.format(table_name))
+        conn.commit()
+        
+        dataframe.to_sql(table_name, conn, if_exists='replace', index=False)
+        
+        conn.close()
+
+def main():
+    make_dataset_dir()
+    make_df()
+    make_db_dir()
+    make_db()
+
+if __name__ == '__main__':
+    main()
